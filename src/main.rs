37894210e5c28650a8440d0e52b98f1e8282be85
Computer <mahmud.hassan@g.bracu.ac.bd>
2023-04-09 16:03:15 +0600

    Repo cleaned

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#![allow(unused)]
+use rand::Rng;
+use std::fs::File;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::cmp::Ordering;
+fn main() {
+    println!("What's your name?")
+}
